06956871498382f09ff6efb38f377c61730c49ba
Helga <helga.varadi1@gmail.com>
2017-03-06 22:07:54 +0100

    additional random answers

--- Random craziness/Bootstrap version/script.js
+++ Random craziness/Bootstrap version/script.js
@@ -12,4 +12,8 @@ var randomCrazy = [
   'Menj oda egy idegenhez és kérd meg, hogy legyen a barátod.',
   'Hívd fel az IT támogatást. Oszd meg velük, hogy pisilned kell.',
   'Ragassz egy darab WC papírt valamelyik cipőtalpadra.'
+  'Hívd fel az IT támogatást és mondd el nekik, hogy magányos vagy.',
+  'Játszd el, hogy anyáddal üvöltözöl telefonon.',
+  'Ölelj meg egy ismeretlent. Aztán állj tovább.',
+  'Versenyezz a lépcsőn lefelé egy idegennel. Utána üvöltsd felé, hogy "győztem!".'
 ]
